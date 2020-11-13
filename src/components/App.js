41b4ff2b3d00f3b288436af401a1918a9d3858e5
HariKishore7 <harikishoret99@gmail.com>
2020-11-13 13:34:58 +0530

    Changes made

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <div>
+      <h1 data-ns-test="project-name" >World jewelery Web app</h1>
+      <p data-ns-test="project-description">Some form of usiness idea</p>
+    </div>
   )
 }
 
